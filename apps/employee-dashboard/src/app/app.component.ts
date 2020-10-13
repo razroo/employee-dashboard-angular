7a766737b39df46cdcadb40329987d0538ebd5cf
Charlie Greenman <charlie@razroo.com>
2020-10-13 14:36:00 +0300

    initial commit

---
+++ apps/employee-dashboard/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'razroo-fully-architected-dashboard-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'employee-dashboard';
+}
