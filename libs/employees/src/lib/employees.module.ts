94f8df10a143dfbf81269cf6631f0eb356635d9a
Charlie Greenman <charlie@razroo.com>
2020-11-18 00:09:23 +0200

    Add an employees component to the app.

--- libs/employees/src/lib/employees.module.ts
+++ libs/employees/src/lib/employees.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EmployeesComponent } from './employees/employees.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [EmployeesComponent],
+  exports: [EmployeesComponent],
 })
 export class EmployeesModule {}
---
+++ libs/employees/src/lib/employees/employees.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'razroo-fully-architected-dashboard-employees',
+  templateUrl: './employees.component.html',
+  styleUrls: ['./employees.component.scss']
+})
+export class EmployeesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
