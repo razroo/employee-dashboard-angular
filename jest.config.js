d67019706b4e6b127709b03c8b22e0172b5f80ce
Charlie Greenman <charlie@razroo.com>
2020-11-18 00:08:33 +0200

    Add the employees lib to the app.

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  projects: ['<rootDir>/apps/employee-dashboard'],
+  projects: ['<rootDir>/apps/employee-dashboard', '<rootDir>/libs/employees'],
 };
---
+++ libs/employees/jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  displayName: 'employees',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: {
+        before: [
+          'jest-preset-angular/build/InlineFilesTransformer',
+          'jest-preset-angular/build/StripStylesTransformer',
+        ],
+      },
+    },
+  },
+  coverageDirectory: '../../coverage/libs/employees',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js',
+  ],
+};
