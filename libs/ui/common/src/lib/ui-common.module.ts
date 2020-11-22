b301980daf70952d94da414cccfa00aed5c85423
Charlie Greenman <charlie@razroo.com>
2020-11-22 19:24:56 +0200

    Add angular material to the app.

--- libs/ui/common/src/lib/ui-common.module.ts
+++ libs/ui/common/src/lib/ui-common.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MatSidenavModule, MatToolbarModule],
   declarations: [LayoutComponent],
   exports: [LayoutComponent],
 })
