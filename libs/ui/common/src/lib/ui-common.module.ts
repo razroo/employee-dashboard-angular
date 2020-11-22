d30a73988178eb52bb7ce04db79cb1512d47ad45
Charlie Greenman <charlie@razroo.com>
2020-11-23 00:35:28 +0200

    Modify data table component and update the favicon.ico.

--- libs/ui/common/src/lib/ui-common.module.ts
+++ libs/ui/common/src/lib/ui-common.module.ts
@@ -5,9 +5,11 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 import { DataTableComponent } from './data-table/data-table.component';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
-  imports: [CommonModule, MatSidenavModule, MatToolbarModule, RouterModule],
+  imports: [CommonModule, MatSidenavModule, MatToolbarModule, RouterModule,
+    MatTableModule],
   declarations: [LayoutComponent, DataTableComponent],
   exports: [LayoutComponent, DataTableComponent],
 })
