0d0a73deea8d0665ee61b93168024181db89ac2d
Charlie Greenman <charlie@razroo.com>
2020-11-22 18:56:43 +0200

    update the employees component so that it uses the layout compoent.

--- libs/employees/src/lib/employees.module.ts
+++ libs/employees/src/lib/employees.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EmployeesComponent } from './employees/employees.component';
 import { EmployeesRoutingModule } from './employees-routing.module';
+import { UiCommonModule } from '@razroo-fully-architected-dashboard/ui/common';
 
 @NgModule({
-  imports: [CommonModule, EmployeesRoutingModule],
+  imports: [CommonModule, UiCommonModule, EmployeesRoutingModule],
   declarations: [EmployeesComponent],
   exports: [EmployeesComponent],
 })
