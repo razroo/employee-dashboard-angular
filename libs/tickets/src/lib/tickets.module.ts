a5e0f621b33b05408276261e6daa47833d92e08d
Charlie Greenman <charlie@razroo.com>
2020-11-24 00:14:13 +0200

    Benchmark progress towards completing the tickets page.

--- libs/tickets/src/lib/tickets.module.ts
+++ libs/tickets/src/lib/tickets.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TicketsComponent } from './tickets/tickets.component';
 import { TicketsRoutingModule } from './tickets-routing.module';
-
+import { UiCommonModule } from '@razroo-fully-architected-dashboard/ui/common';
 
 @NgModule({
-  imports: [CommonModule, TicketsRoutingModule],
+  imports: [CommonModule, TicketsRoutingModule, UiCommonModule],
   declarations: [TicketsComponent],
 })
 export class TicketsModule {}
--- libs/tickets/src/lib/tickets/tickets.component.ts
+++ libs/tickets/src/lib/tickets/tickets.component.ts
@@ -1,4 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { TableColumn } from '@razroo-fully-architected-dashboard/ui/common';
+
+const ELEMENT_DATA = [
+  {name: 'test', position: 'test position', location: 'New York',
+    permission: 'test permission', reportTo: 'no one'},
+  {name: 'test', position: 'test position', location: 'Los Angelos',
+    permission: 'test permission', reportTo: 'no one'},
+  {name: 'test', position: 'test position', location: 'Alabama',
+    permission: 'test permission', reportTo: 'no one'},
+  {name: 'test', position: 'test position', location: 'new york',
+    permission: 'test permission', reportTo: 'no one'},
+  {name: 'test', position: 'test position', location: 'new york',
+    permission: 'test permission', reportTo: 'no one'},
+  {name: 'test', position: 'test position', location: 'new york',
+    permission: 'test permission', reportTo: 'no one'},
+  {name: 'test', position: 'test position', location: 'new york',
+    permission: 'test permission', reportTo: 'no one'},
+  {name: 'test', position: 'test position', location: 'new york',
+    permission: 'test permission', reportTo: 'no one'},
+];
 
 @Component({
   selector: 'razroo-fully-architected-dashboard-tickets',
@@ -6,10 +26,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tickets.component.scss']
 })
 export class TicketsComponent implements OnInit {
+  elementData;
+  ticketsTableColumns: TableColumn[];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.elementData = ELEMENT_DATA;
+
+    this.ticketsTableColumns = [
+      {
+        name: 'Name',
+        dataKey: 'name',
+        isSortable: true,
+      },
+      {
+        name: 'Position',
+        dataKey: 'position',
+        isSortable: true,
+      },
+      {
+        name: 'Location',
+        dataKey: 'location',
+        isSortable: true,
+      },
+      {
+        name: 'Permission',
+        dataKey: 'permission',
+        isSortable: true,
+      },
+      {
+        name: 'Report To',
+        dataKey: 'reportTo',
+        isSortable: true,
+      }
+    ];
   }
 
 }
