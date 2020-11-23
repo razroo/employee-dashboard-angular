2cde0961825b0a61f0e697eb7040d90df5cfb271
Charlie Greenman <charlie@razroo.com>
2020-11-23 23:19:24 +0200

    Benchmark progress towards adding a data table to the repo-data page.

--- libs/repo-data/src/lib/repo-data.module.ts
+++ libs/repo-data/src/lib/repo-data.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RepoDataComponent } from './repo-data/repo-data.component';
 import { RepoDataRoutingModule } from './repo-data/repo-data-routing.module';
+import { UiCommonModule } from '@razroo-fully-architected-dashboard/ui/common';
 
 @NgModule({
-  imports: [CommonModule, RepoDataRoutingModule],
+  imports: [CommonModule, RepoDataRoutingModule, UiCommonModule],
   declarations: [RepoDataComponent],
 })
 export class RepoDataModule {}
--- libs/repo-data/src/lib/repo-data/repo-data.component.ts
+++ libs/repo-data/src/lib/repo-data/repo-data.component.ts
@@ -1,4 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { TableColumn } from '@razroo-fully-architected-dashboard/ui/common';
+
+const ELEMENT_DATA = [
+  {name: 'test', position: 'test position', location: 'New York',
+    permission: 'test permission', reportTo: 'no one'},
+  {name: 'test', position: 'test position', location: 'Los Angelos',
+    permission: 'test permission', reportTo: 'no one'},
+  {name: 'test', position: 'test position', location: 'Alabama',
+    permission: 'test permission', reportTo: 'no one'},
+  {name: 'test', position: 'test position', location: 'new york',
+    permission: 'test permission', reportTo: 'no one'},
+  {name: 'test', position: 'test position', location: 'new york',
+    permission: 'test permission', reportTo: 'no one'},
+  {name: 'test', position: 'test position', location: 'new york',
+    permission: 'test permission', reportTo: 'no one'},
+  {name: 'test', position: 'test position', location: 'new york',
+    permission: 'test permission', reportTo: 'no one'},
+  {name: 'test', position: 'test position', location: 'new york',
+    permission: 'test permission', reportTo: 'no one'},
+];
 
 @Component({
   selector: 'razroo-fully-architected-dashboard-repo-data',
@@ -6,10 +26,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repo-data.component.scss']
 })
 export class RepoDataComponent implements OnInit {
+  elementData;
+  repoDataTableColumns: TableColumn[];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.elementData = ELEMENT_DATA;
+
+    this.repoDataTableColumns = [
+      {
+        name: 'Name',
+        dataKey: 'name',
+        isSortable: true,
+      },
+      {
+        name: 'Position',
+        dataKey: 'position',
+        isSortable: true,
+      },
+      {
+        name: 'Location',
+        dataKey: 'location',
+        isSortable: true,
+      },
+      {
+        name: 'Permission',
+        dataKey: 'permission',
+        isSortable: true,
+      },
+      {
+        name: 'Report To',
+        dataKey: 'reportTo',
+        isSortable: true,
+      }
+    ];
   }
 
 }
