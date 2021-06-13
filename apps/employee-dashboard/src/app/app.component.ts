f7db9818a9f541f3c6be4d77fdf45c5cda133576
Tommy <tommymaher10@gmail.com>
2021-06-13 21:00:54 +0100

    Big Commit! :rocket:
    Fixed Angular version difference.
    
    *Added Nav elements
    
    *added toggle for side container

--- apps/employee-dashboard/src/app/app.component.ts
+++ apps/employee-dashboard/src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
-
+import { faBell } from '@fortawesome/free-solid-svg-icons';
+import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { faUserFriends } from '@fortawesome/free-solid-svg-icons';
+import { faChartPie } from '@fortawesome/free-solid-svg-icons';
+import { faClipboardList } from '@fortawesome/free-solid-svg-icons';
+import { faChartLine } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'razroo-fully-architected-dashboard-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
+
+
+
 export class AppComponent {
   title = 'Razroo | Fully Architected Employee Dashboard';
+
+
+
+ faBell = faBell;
+ faQuestionCircle = faQuestionCircle; 
+ faUserCircle = faUserCircle
+
+ faUserFriends = faUserFriends
+ faChartPie = faChartPie;
+ faClipboardList = faClipboardList;
+ faChartLine = faChartLine;
+
+ showFiller = false;
+
+ 
 }
