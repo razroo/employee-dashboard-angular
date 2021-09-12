a0b2746a708cd32171a8bc6d4738c00f85b6daf3
picojp32 <Jonathan.Picovsky@gmail.com>
2021-09-12 11:24:00 +0300

    add tickets querry update .ignore

---
+++ libs/data-graphql/src/lib/tickets/tickets.queries.ts
@@ -0,0 +1,16 @@
+import gql from 'graphql-tag';
+
+export const TicketsQuery = gql`
+query getTickets ( $start: Int, $limit: Int, $sort: String, $where: JSON ) {
+    tickets (start: $start, limit: $limit, sort: $sort, where: $where) {
+      id
+      employeeName
+      projectName
+      description
+      ticketName
+      ticketLink
+      reporter
+      designLink
+  }
+}
+`;
