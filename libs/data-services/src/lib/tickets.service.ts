ec146ad8ed599d71fd53726e27019e1c480a3040
Charlie Greenman <charlie@razroo.com>
2021-09-22 20:18:40 +0300

    Update the tickets effects.

--- libs/data-services/src/lib/tickets.service.ts
+++ libs/data-services/src/lib/tickets.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { from, Observable } from 'rxjs'
-import { pluck,   } from 'rxjs/operators';
+import { from, Observable} from 'rxjs'
+import { pluck} from 'rxjs/operators';
 import { Apollo } from 'apollo-angular';
 import { TicketsQuery } from '@razroo-fully-architected-dashboard/data-graphql';
 
@@ -12,19 +12,15 @@ export class TicketsService {
   constructor(public apollo: Apollo) { }
 
   getTickets(event?:any, sortEvent?: any): Observable<TicketsService> {
-    console.log('event');
-
-
     const query = TicketsQuery
-    const allTickets = this.apollo.query({
-      query:query,
-      variables: {
-        start: event ? event.pageIndex * event.pageSize: 0,
-        limit:  event ? event.pageSize  : 10,
-        sort: 'employeeName:asc,employeeName:desc',
-        where: event ? {employeeName_contains: event.where} : {}
-      }
-     })
+    const variables = {
+      start: event ? event.pageIndex * event.pageSize: 0,
+      limit:  event ? event.pageSize  : 10,
+      sort: 'employeeName:asc,employeeName:desc',
+      where: event ? {employeeName_contains: event.where} : {}
+    }
+
+    const allTickets = this.apollo.query({query, variables})
 
     return from(allTickets).pipe(pluck('data', 'tickets'));
 
